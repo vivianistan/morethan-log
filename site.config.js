97e52b179af9abe8e34b21b8236df7a9ac83916e
Vivian Tan <Vivian.Tan@ibm.com>
2024-02-19 20:41:55 -0600

    update revalidateTime

--- site.config.js
+++ site.config.js
@@ -24,7 +24,7 @@ const CONFIG = {
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
+  link: "",
   since: 2024, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
@@ -69,7 +69,8 @@ const CONFIG = {
     },
   },
   isProd: process.env.VERCEL_ENV === "production", // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
-  revalidateTime: 21600 * 7, // revalidate time for [slug], index
+  // revalidateTime: 21600 * 7, // revalidate time for [slug], index
+  revalidateTime: 30, // revalidate time for [slug], index
 }
 
 module.exports = { CONFIG }
